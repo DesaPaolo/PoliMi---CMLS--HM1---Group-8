c25eaff7224726b15d4abcf2e2810f7177720520
Paolo De Santis <pds9662@gmail.com>
2020-04-18 17:40:28 +0200

    feature extraction + data to txt handling + code refactoring

---
+++ classifier.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+
+# Load feature vector
+path = "C:/Users/Paolo De Santis/Desktop/UrbanSound/Feature coeff csv/"
+x_train = np.genfromtxt(path + '1-9folds_mfcc_n_coeff=20_x_train.csv', delimiter=',')
+x_test = np.genfromtxt(path + 'fold10_mfcc_20_coeff_x_test.csv', delimiter=',')
+y_train = np.genfromtxt(path + '1-9folds_mfcc_20_coeff_y_train.csv', delimiter=',')
+y_test = np.genfromtxt(path + 'fold10_mfcc_20_coeff_y_test.csv', delimiter=',')
+
+
+scaler = StandardScaler()
+scaler.fit(x_train)
+x_train_scaled = scaler.transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+# Classifier
+grid_params = {
+    'n_neighbors': [3, 5, 7, 9, 11, 15],
+    'weights': ['uniform', 'distance'],
+    'metric': ['euclidean', 'manhattan']
+}
+
+model = GridSearchCV(KNeighborsClassifier(), grid_params, cv=5, n_jobs=-1)
+model.fit(x_train_scaled, y_train)
+y_predict = model.predict(x_test_scaled)
+
+#Evaluation
+accuracy = model.score(x_test_scaled, y_test)#x_test_normalized/x_test
+print("Accuracy:")
+print(accuracy)
+print(confusion_matrix(y_test, y_predict))
+
---
+++ feature_extraction.py
@@ -0,0 +1,72 @@
+import librosa
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# 0 = air_conditioner
+# 1 = car_horn
+# 2 = children_playing
+# 3 = dog_bark
+# 4 = drilling
+# 5 = engine_idling
+# 6 = gun_shot
+# 7 = jackhammer
+# 8 = siren
+# 9 = street_music
+
+# DATA HANDLING
+path = "C:/Users/Paolo De Santis/Desktop/UrbanSound/UrbanSound8K"  # /Users/PilvioSol/Desktop/UrbanSound8K
+
+# Read metadata file
+df = pd.read_csv(path + "/metadata/UrbanSound8K.csv")
+
+# # Reorganize df (choose folds in if) ---> if you want 10 folds comment this for
+# data = []
+# for i in tqdm(range(len(df))):
+#     fold_no = str(df.iloc[i]["fold"])
+#     if (fold_no == '1' or fold_no == '2'):  # or fold_no == '3'):
+#         data.append(df.iloc[i])
+#
+# df = pd.DataFrame(data)
+
+# FEATURE EXTRACTION
+x_train = []  # feature coeffs
+x_test = []  # feature coeffs
+y_train = []  # labels
+y_test = []  # labels (desidered outputs)
+
+def parse_audio(x):
+    return x.flatten('F')[:x.shape[0]]
+
+test_fold = '10'
+
+for i in tqdm(range(len(df))):
+    fold_no = str(df.iloc[i]["fold"])
+    file = df.iloc[i]["slice_file_name"]
+    label = df.iloc[i]["classID"]
+    filename = path + "/audio/fold" + fold_no + "/" + file
+    y, sr = librosa.load(filename)  # convert to mono
+    y = parse_audio(y)
+
+    # MFFCs
+    mfccs = np.mean(librosa.feature.mfcc(y, sr).T, axis=0)  # n_mfcc = 20 (default value)
+
+    if (fold_no != test_fold):
+        x_train.append(mfccs)  # features
+        y_train.append(label)
+    else:
+        x_test.append(mfccs)  # features
+        y_test.append(label)
+
+# Converting the lists into numpy arrays
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+# Load feature vectors in file .csv
+path = "C:/Users/Paolo De Santis/Desktop/UrbanSound/Feature coeff csv/"
+np.savetxt(path + '1-9folds_mfcc_n_coeff=20_x_train.csv', x_train, delimiter=',')
+np.savetxt(path + 'fold10_mfcc_20_coeff_x_test.csv', x_test, delimiter=',')
+np.savetxt(path + '1-9folds_mfcc_20_coeff_y_train.csv', y_train, delimiter=',')
+np.savetxt(path + 'fold10_mfcc_20_coeff_y_test.csv', y_test, delimiter=',')
